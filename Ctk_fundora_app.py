c47457a5863f2247891c845050747c5403617dc2
Kasper Larsson <contactkasperlarsson@gmail.com>
2025-09-18 08:58:09 +0200

    These are the changes.

--- Ctk_fundora_app.py
+++ Ctk_fundora_app.py
@@ -1,5 +1,9 @@
 # Lav ny Class hver gang den skal kaldes fra main scriptet. Image_output Image_Import
 # brug alle udnerfoldere 
+
+# checking for ignore files. 
+
+
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".")))
